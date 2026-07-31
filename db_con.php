a61e08a71a5cfb679116a153a65d99671b1c99c1
YAHSFA MUSTAFA <yashfamustafa04@gmail.com>
2026-07-31 20:18:01 +0500

    Update database connection details

--- db_con.php
+++ db_con.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "employee";
+$servername = "sql108.infinityfree.com";
+$username = "if0_42530887";
+$password = "mynameisyashfa";
+$dbname = "if0_42530887_Employees";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
